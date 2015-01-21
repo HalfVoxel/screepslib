5e749dfae963ac67f8f5f76a8900f5b8e21bfa43
Aron Granberg <aron.granberg@gmail.com>
2015-01-21 23:55:49 +0100

    Updated to reflect changes in the API.

--- Game.hx
+++ Game.hx
@@ -6,10 +6,19 @@ extern class Game {
 	public static var creeps : DynamicObject<Creep>;
     public static var flags : DynamicObject<Flag>;
     public static var spawns : DynamicObject<Spawn>;
+    public static var rooms : DynamicObject<Room>;
     public static var structures : DynamicObject<Structure>;
     public static var time : Int;
 
-    public static function getRoom ( name : String ) : Maybe<Room>;
+    @:deprecated
+    public static inline function getRoom ( name : String ) : Maybe<Room> {
+    	var res = null;
+    	for (room in Game.rooms) {
+    		res = room;
+    	}
+    	return res;
+    }
+
    	public static function notify ( message : String ) : Void;
    	public static function getObjectById ( id : String ) : Maybe<Entity>;
 }
