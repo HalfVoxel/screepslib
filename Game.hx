6a9c9fc903f7eec4566d047cfed59b96b9a8ad26
Aron Granberg <aron.granberg@gmail.com>
2015-01-23 17:16:03 +0100

    Added cpuLimit and getUsedCpu

--- Game.hx
+++ Game.hx
@@ -3,22 +3,24 @@ package;
 import maybe.Maybe;
 
 extern class Game {
-	public static var creeps : DynamicObject<Creep>;
-    public static var flags : DynamicObject<Flag>;
-    public static var spawns : DynamicObject<Spawn>;
-    public static var rooms : DynamicObject<Room>;
-    public static var structures : DynamicObject<Structure>;
-    public static var time : Int;
+	public static var creeps (default, null) : DynamicObject<Creep>;
+	public static var flags (default, null) : DynamicObject<Flag>;
+	public static var spawns (default, null) : DynamicObject<Spawn>;
+	public static var rooms (default, null) : DynamicObject<Room>;
+	public static var structures (default, null) : DynamicObject<Structure>;
+	public static var time (default, null) : Int;
+	public static var cpuLimit (default, null) : Float;
 
-    @:deprecated
-    public static inline function getRoom ( name : String ) : Maybe<Room> {
-    	var res = null;
-    	for (room in Game.rooms) {
-    		res = room;
-    	}
-    	return res;
-    }
+	@:deprecated
+	public static inline function getRoom ( name : String ) : Maybe<Room> {
+		var res = null;
+		for (room in Game.rooms) {
+			res = room;
+		}
+		return res;
+	}
 
-   	public static function notify ( message : String ) : Void;
-   	public static function getObjectById ( id : String ) : Maybe<Entity>;
+	public static function notify ( message : String ) : Void;
+	public static function getObjectById ( id : String ) : Maybe<Entity>;
+	public static function getUsedCpu ( callback : ( Float -> Void) ) : Void;
 }
